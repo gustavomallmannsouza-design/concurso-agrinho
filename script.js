5a2d03605167016649fa66a29f31c3cc91c63283
gustavomallmannsouza-design <gustavo.mallmann.souza@escola.pr.gov.br>
2026-06-02 11:24:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// scripts.js
 
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("botao");
+    const mensagem = document.getElementById("mensagem");
+
+    botao.addEventListener("click", () => {
+        mensagem.textContent = "Olá! Você clicou no botão.";
+        mensagem.style.color = "blue";
+    });
+});
